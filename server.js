95d58db4a1080ad1fa7837e856b5cf77cc7fa880
Itzik Miro <Miro.Itzik@gmail.com>
2020-04-13 11:30:39 +0300

    test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
-	console.log(q);
+	console.log(query);
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
 	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
