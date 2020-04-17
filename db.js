3d5b7d6efafd3b48d1ca06b95fe9488ed8312f68
Itzik Miro <Miro.Itzik@gmail.com>
2020-04-17 20:21:29 +0300

    Convert CRUD request to use current user

--- db.js
+++ db.js
@@ -23,7 +23,7 @@ db.sequelize = sequelize;
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.todo.belongsTo(db.user.id, { foreignKey: 'user' });
+db.todo.belongsTo(db.user.id);
 db.user.hasMany(db.todo);
 
 module.exports = db;
